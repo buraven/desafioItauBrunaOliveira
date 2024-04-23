4f2916a0821af65702dbac054b4ac3ed32434545
buraven <brunaoliemp@gmail.com>
2024-04-23 19:47:39 -0300

    feat: ajuste no circuitBreaker

--- src/main/java/com/desafioitau/api/transferencia/domain/service/impl/CadastroServiceImpl.java
+++ src/main/java/com/desafioitau/api/transferencia/domain/service/impl/CadastroServiceImpl.java
@@ -1,11 +1,14 @@
 package com.desafioitau.api.transferencia.domain.service.impl;
 
+import com.desafioitau.api.transferencia.CircuitBreakerLogConfig;
 import com.desafioitau.api.transferencia.domain.service.CadastroService;
 import com.desafioitau.api.transferencia.dto.ClienteResponseDTO;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,18 +16,42 @@ import java.util.Map;
 @Service
 public class CadastroServiceImpl implements CadastroService {
 
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerLogConfig.class);
+    private final RestTemplate restTemplate;
+
     private final Map<String, ClienteResponseDTO> CACHE = new HashMap<>();
+
+    public CadastroServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     @CircuitBreaker(name = "buscarClientePorIdCB", fallbackMethod = "buscarClientePorIdNoCache")
     public ClienteResponseDTO buscarClientePorId(String id) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<ClienteResponseDTO> response = restTemplate
-                .getForEntity("http://localhost:9090/clientes/" + id, ClienteResponseDTO.class);
+        return executarRequisicao(id);
+    }
 
-        CACHE.put(id, response.getBody());
+    private ClienteResponseDTO executarRequisicao(String id) {
+        final String API_URL = UriComponentsBuilder
+                .fromHttpUrl("http://localhost:9090/clientes/" + id)
+                .encode()
+                .toUriString();
 
-        return response.getBody();
+        logger.info("Buscando cliente por id");
+        final ClienteResponseDTO clienteResponseDTO;
+        try {
+            clienteResponseDTO = restTemplate
+                    .getForEntity(API_URL, ClienteResponseDTO.class)
+                    .getBody();
+        } catch (Exception e) {
+            logger.error("Erro ao buscar cliente por id");
+            throw e;
+        }
+
+        logger.info("Populando cache");
+        CACHE.put(id, clienteResponseDTO);
+
+        return clienteResponseDTO;
     }
 
     private ClienteResponseDTO buscarClientePorIdNoCache(String id) {
--- src/main/java/com/desafioitau/api/transferencia/domain/service/impl/ContaServiceImpl.java
+++ src/main/java/com/desafioitau/api/transferencia/domain/service/impl/ContaServiceImpl.java
@@ -1,12 +1,18 @@
 package com.desafioitau.api.transferencia.domain.service.impl;
 
+import com.desafioitau.api.transferencia.CircuitBreakerLogConfig;
 import com.desafioitau.api.transferencia.domain.service.ContaService;
+import com.desafioitau.api.transferencia.dto.ClienteResponseDTO;
 import com.desafioitau.api.transferencia.dto.ContaResponseDTO;
+import com.desafioitau.api.transferencia.dto.NotificacaoRequestDTO;
 import com.desafioitau.api.transferencia.dto.SaldoRequestDTO;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,28 +20,59 @@ import java.util.Map;
 @Service
 public class ContaServiceImpl implements ContaService {
 
-    private final Map<String, ContaResponseDTO> CACHE = new HashMap<>();
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerLogConfig.class);
+    private final RestTemplate restTemplate;
+
+    public ContaServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     @CircuitBreaker(name = "buscarContaPorIdCB", fallbackMethod = "buscarContaPorIdNoCache")
     public ContaResponseDTO buscarContaPorId(String id) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<ContaResponseDTO> response = restTemplate
-                .getForEntity("http://localhost:9090/contas/" + id, ContaResponseDTO.class);
-
-        return response.getBody();
+        return executarRequisicao(id);
     }
 
     @Override
     @CircuitBreaker(name = "atualizarSaldoCB")
     public void atualizarSaldo(SaldoRequestDTO saldoRequestDTO) {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate
-                .put("http://localhost:9090/contas/saldos", saldoRequestDTO);
+        executarRequisicao(saldoRequestDTO);
     }
 
-    private ContaResponseDTO buscarContaPorIdNoCache(String id) {
-        return CACHE.getOrDefault(id, new ContaResponseDTO());
+    private ContaResponseDTO executarRequisicao(String id) {
+        final String API_URL = UriComponentsBuilder
+                .fromHttpUrl("http://localhost:9090/contas/" + id)
+                .encode()
+                .toUriString();
+
+        logger.info("Buscando conta por id");
+        final ContaResponseDTO contaResponseDTO;
+        try {
+            contaResponseDTO = restTemplate
+                    .getForEntity(API_URL, ContaResponseDTO.class)
+                    .getBody();
+        } catch (Exception e) {
+            logger.error("Erro ao buscar conta por id");
+            throw e;
+        }
+
+        return contaResponseDTO;
+    }
+
+    private void executarRequisicao(SaldoRequestDTO saldoRequestDTO) {
+        final String API_URL = UriComponentsBuilder
+                .fromHttpUrl("http://localhost:9090/contas/saldos")
+                .encode()
+                .toUriString();
+
+        logger.info("Atualizar saldo apos tranferencia");
+        try {
+            restTemplate
+                    .put(API_URL, saldoRequestDTO);
+        } catch (Exception e) {
+            logger.error("Erro ao atualizar saldo apos tranferencia");
+            throw e;
+        }
     }
 
 }
